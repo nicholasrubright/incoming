5f09002ea36023a4498821a913e4502873089f87
nicholasrubright <nkrubright@gmail.com>
2024-12-29 05:05:29 -0500

    initla commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center">
+        <h1 className="text-3xl font-bold underline">App!!</h1>
+      </div>
+    </div>
+  );
+}
